4099b015f5c4dda0c0227897617d5e17d850ea4f
Bugra Ahmet Caglar <bugraahmetcaglar@gmail.com>
2020-05-05 01:51:12 +0300

    fixed bugs

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -4,26 +4,30 @@ from django.urls import path
 from adminpanel.views import views, groups, users
 
 urlpatterns = [
-    path('giris/', views.login_admin, name="login_admin"),
-    path('cikis/', views.logout_admin, name="logout_admin"),
+    # Main View
+    path('login/', views.login_admin, name="login_admin"),
+    path('logout/', views.logout_admin, name="logout_admin"),
+    path('unread-tickets/', views.admin_unread_tickets, name="admin_unread_tickets"),
+    path('ticket/detail/<int:ticketNumber>', views.admin_ticket_detail, name="admin_ticket_detail"),
+    path('reply/<int:ticketNumber>/', views.admin_add_reply, name="admin_add_reply"),
+    path('ticket/delete/<int:ticketNumber>', views.admin_delete_ticket, name="admin_delete_ticket"),
+    path('', views.admin_index, name="admin_index"),
+    path('all-tickets/', views.admin_all_tickets, name="admin_all_tickets"),
 
+    # User
     path('users/group/add/', users.admin_add_account_group, name="admin_add_account_group"),
     path('users/group/', users.admin_account_groups, name="admin_account_groups"),
     path('users/new/', users.admin_add_account, name="admin_add_account"),
     path('users/add-group-to-user/', users.admin_add_group_to_user, name="admin_add_group_to_user"),
     path('users/', users.admin_users, name="admin_users"),
-    path('unread-tickets/', views.admin_unread_tickets, name="admin_unread_tickets"),
-    path('ticket/detail/<int:ticketNumber>', views.admin_ticket_detail, name="admin_ticket_detail"),
-    path('reply/<int:ticketNumber>/', views.admin_add_reply, name="admin_add_reply"),
-    path('ticket/delete/<int:ticketNumber>', views.admin_delete_ticket, name="admin_delete_ticket"),
 
     url(r'^edit-account/(?P<username>[\w-]+)/$', users.admin_edit_account, name="admin_edit_account"),
     url(r'^block-account/(?P<username>[\w-]+)/$', users.admin_block_account, name="admin_block_account"),
 
-    path('', views.admin_index, name="admin_index"),
-    path('all-tickets/', views.admin_all_tickets, name="admin_all_tickets"),
+    # Group
     path('groups/', groups.admin_all_groups, name="admin_all_groups"),
     path('add-group/', groups.admin_add_group, name="admin_add_group"),
     path('edit-group/<slug:slug>', groups.admin_edit_group, name="admin_edit_group"),
     path('delete-group/<slug:slug>', groups.admin_delete_group, name="admin_delete_group"),
+
 ]
